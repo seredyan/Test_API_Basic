8ce517f91935834d67354540bf7e17a3b47aeafe
Sergey Sereda <83193859+seredyan@users.noreply.github.com>
2022-02-07 13:02:02 -0800

    there was added logging

--- lib/my_requests.py
+++ lib/my_requests.py
@@ -1,4 +1,5 @@
 import requests
+from lib.logger import Logger
 
 class MyRequests():
     @staticmethod
@@ -30,6 +31,9 @@ class MyRequests():
         if cookies is None:
             cookies = {}
 
+        Logger.add_request(url, data, headers, cookies, method)
+
+
         if method == 'GET':
             response = requests.get(url, params=data, headers=headers, cookies=cookies)
         elif method == 'POST':
@@ -40,5 +44,7 @@ class MyRequests():
             response = requests.delete(url, data=data, headers=headers, cookies=cookies)
         else:
             raise Exception(f"Bad HTTP method '{method}' was received")
+
+        Logger.add_response(response)
 
         return response
